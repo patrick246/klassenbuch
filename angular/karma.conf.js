a2188fe9ef3ee8a9e3a73b72c078aa3cf3fdc2d4
Patrick Hahn <patricksilashahn@gmx.net>
2017-09-11 13:12:17 +0200

    unit testing with phantomjs and junit files

--- angular/karma.conf.js
+++ angular/karma.conf.js
@@ -10,7 +10,9 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
+      require('karma-phantomjs-launcher'),
+      require('karma-junit-reporter')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -32,13 +34,18 @@ module.exports = function (config) {
       environment: 'dev'
     },
     reporters: config.angularCli && config.angularCli.codeCoverage
-              ? ['progress', 'coverage-istanbul']
-              : ['progress', 'kjhtml'],
+              ? ['progress', 'junit', 'coverage-istanbul']
+              : ['progress', 'junit', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    browsers: ['PhantomJS'],
+    singleRun: false,
+    junitReporter: {
+      outputDir: 'testresults',
+      outputFile: 'test-results.xml',
+      useBrowserName: false
+    }
   });
 };
